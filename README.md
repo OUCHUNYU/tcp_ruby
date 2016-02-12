7cbdd5ebaa794f699bb62f8b229666745b5b37e9
OUCHUNYU <ouchunyu@yahoo.com>
2016-02-11 17:33:19 -0800

    Add ruby chatting function

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ruby-networking
 A practice of Ruby socket programming following the sample code in my Ruby book.
+
+- Newly added a chating feature written in Ruby. Under directory ruby-chat
---
+++ ruby-chat/ruby_chat_client.rb
@@ -0,0 +1,34 @@
+require "socket"
+
+class Client
+  def initialize(server)
+    @server = server     # create server
+    @request = nil       # each time refresh
+    @response = nil
+    listen               # call the funtion
+    send                 # call another function
+    @request.join        # make sure everything runs untill the end
+    @response.join
+  end
+
+  def listen
+    @response = Thread.new do      # For each function we create a new thread so
+      loop {                       # that a user can use all functions at the same
+        msg = @server.gets.chomp   # time. Otherwise, the user can send message only
+        puts "#{msg}"              # after the other user has sent back something.
+      }
+    end
+  end
+
+  def send
+    @request = Thread.new do
+      loop {                       # Both loops forever.
+        msg = $stdin.gets.chomp
+        @server.puts "#{msg}"
+      }
+    end
+  end
+end
+
+server = TCPSocket.open( "localhost", 3366 )   # Connecting to server
+Client.new(server)
